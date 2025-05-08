54ea15e3d7be254d90ed2b717280e20ea7243b90
peter-zx <1420229579@qq.com>
2025-05-08 18:44:02 +0800

    修复服务器解析失败问题，环境配置少安装了一个

--- app.py
+++ app.py
@@ -99,4 +99,4 @@ def save_records(results):
             print(f"保存记录 {name} 失败：{str(e)}")
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=5000)
+    app.run(debug=True, host='0.0.0.0', port=1575)
